668dc1e7ee3f683b082ace64b4f6b8468840d309
Syr0kx <sven-spieler@web.de>
2016-12-19 16:00:03 +0100

    #11 username will now be inserted into users LOL

--- register.php
+++ register.php
@@ -70,8 +70,8 @@ if(isset($_GET['register'])) {
 	if(!$error) {	
 		$passwort_hash = password_hash($passwort, PASSWORD_DEFAULT);
 		
-		$statement = $pdo->prepare("INSERT INTO users (email, passwort, vorname, nachname) VALUES (:email, :passwort, :vorname, :nachname)");
-		$result = $statement->execute(array('email' => $email, 'passwort' => $passwort_hash, 'vorname' => $vorname, 'nachname' => $nachname));
+		$statement = $pdo->prepare("INSERT INTO users (username, email, passwort, vorname, nachname) VALUES (:username, :email, :passwort, :vorname, :nachname)");
+		$result = $statement->execute(array('username' => $username, 'email' => $email, 'passwort' => $passwort_hash, 'vorname' => $vorname, 'nachname' => $nachname));
 		
 		if($result) {		
 			echo 'Du wurdest erfolgreich registriert. <a href="index.php">Zum Login</a>';
