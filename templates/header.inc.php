8fd8b57cbf3f464c5a394495a82729431894d4a1
Syr0kx <sven-spieler@web.de>
2016-12-19 15:23:27 +0100

    #5

--- templates/header.inc.php
+++ templates/header.inc.php
@@ -26,7 +26,7 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-          <a class="navbar-brand" href="internal.php"><i class="glyphicon glyphicon-leaf logo"></i>Instagap</a>
+          <a class="navbar-brand" href="internal.php"><i class="glyphicon glyphicon-camera logo"></i>Instagap</a>
         </div>
         <?php if(!is_checked_in()): ?>
         <div id="navbar" class="navbar-collapse collapse">
