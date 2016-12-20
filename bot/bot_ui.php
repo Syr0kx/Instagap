7c82adfee341e01885b8e93fcdd250cb0d15f18d
Fynn <fynn.merlevede@googlemail.com>
2016-12-20 16:42:35 +0100

    bot changes

--- bot/bot_ui.php
+++ bot/bot_ui.php
@@ -35,6 +35,9 @@ $yourfollowers=0;
             <div class="ui buttons"> 
                 <button class="ui button green">  running</button>
                 <button class="ui button loading"></button>
+                <label>your followers: <?php echo $yourfollowers; ?></label>
+
+
             </div> 
         </div>
     </div>
