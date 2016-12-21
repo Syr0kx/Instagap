6aec32d93f874b9057d15e19d45affe9d1e33000
Fynn Merlevede <fynn.merlevede@googlemail.com>
2016-12-21 12:40:59 +0100

    logic changed

--- bot/bot_logic.php
+++ bot/bot_logic.php
@@ -10,10 +10,6 @@ $running = false;
 $yourfollowers=0;
 //////////////////////
 
-// THIS IS AN EXAMPLE OF HOW TO USE NEXT_MAX_ID TO PAGINATE
-// IN THIS EXAMPLE, WE ARE RETRIEVING SELF FOLLOWERS
-// BUT THE PROCESS IS SIMILAR IN OTHER REQUESTS
-
 $i = new \InstagramAPI\Instagram($debug);
 
 $i->setUser($username, $password);
